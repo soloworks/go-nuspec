a9e5bdff109872fa666da21f635b450d9fe5740e
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-10-15 14:52:17 +0100

    Fixed formatting to match original nuget output

--- nuspec.go
+++ nuspec.go
@@ -5,34 +5,43 @@ import (
 	"encoding/xml"
 )
 
+// Dependency is used in the File struct
+type Dependency struct {
+	XMLName xml.Name `xml:"dependency"`
+	ID      string   `xml:"id,attr"`
+	Version string   `xml:"version,attr"`
+}
+
 // File Represents a .nuspec XML file found in the root of the .nupck files
 type File struct {
-	Package struct {
-		Xmlns string `xml:"xmlns"`
-	} `xml:"package"`
-	Metadata struct {
-		ID                       string `xml:"id"`
-		Version                  string `xml:"version"`
-		Title                    string `xml:"title"`
-		Authors                  string `xml:"authors"`
-		Owners                   string `xml:"owners"`
-		ProjectURL               string `xml:"projectUrl"`
-		LicenseURL               string `xml:"licenseUrl"`
-		IconURL                  string `xml:"iconUrl"`
-		RequireLicenseAcceptance string `xml:"requireLicenseAcceptance"`
-		Description              string `xml:"description"`
-		ReleaseNotes             string `xml:"releaseNotes"`
-		Copyright                string `xml:"copyright"`
-		Summary                  string `xml:"summary"`
-		Language                 string `xml:"language"`
-		Tags                     string `xml:"tags"`
+	XMLName xml.Name `xml:"package"`
+	Xmlns   string   `xml:"xmlns,attr,omitempty"`
+	Meta    struct { // MetaData
+		ID               string `xml:"id"`
+		Version          string `xml:"version"`
+		Title            string `xml:"title,omitempty"`
+		Authors          string `xml:"authors"`
+		Owners           string `xml:"owners,omitempty"`
+		LicenseURL       string `xml:"licenseUrl,omitempty"`
+		ProjectURL       string `xml:"projectUrl,omitempty"`
+		IconURL          string `xml:"iconUrl,omitempty"`
+		ReqLicenseAccept bool   `xml:"requireLicenseAcceptance"`
+		Description      string `xml:"description"`
+		ReleaseNotes     string `xml:"releaseNotes,omitempty"`
+		Copyright        string `xml:"copyright,omitempty"`
+		Summary          string `xml:"summary,omitempty"`
+		Language         string `xml:"language,omitempty"`
+		Tags             string `xml:"tags,omitempty"`
+		Dependencies     struct {
+			Dependency []Dependency
+		} `xml:"dependencies,omitempty"`
 	} `xml:"metadata"`
 }
 
 // New returns a populated skeleton for a Nuget Packages request (/Packages)
 func New() *File {
 	nsf := File{}
-	nsf.Package.Xmlns = `http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd`
+	nsf.Xmlns = `http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd`
 	return &nsf
 }
 
@@ -56,9 +65,16 @@ func FromBytes(b []byte) (*File, error) {
 func (nsf *File) ToBytes() ([]byte, error) {
 	var b bytes.Buffer
 	// Unmarshal into XML
-	output, err := xml.MarshalIndent(nsf, "  ", "    ")
+	output, err := xml.MarshalIndent(nsf, "", "  ")
 	if err != nil {
 		return nil, err
+	}
+	// Self-Close any empty XML elements (to match original Nuget output)
+	// This assumes Indented Marshalling above, non Indented will break XML
+	for bytes.Contains(output, []byte(`></`)) {
+		i := bytes.Index(output, []byte(`></`))
+		j := bytes.Index(output[i+1:], []byte(`>`))
+		output = append(output[:i], append([]byte(` /`), output[i+j+1:]...)...)
 	}
 	// Write the XML Header
 	b.WriteString(xml.Header)
